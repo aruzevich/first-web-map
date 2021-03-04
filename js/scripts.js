763c1a73bd3c821a4af35eaa6254702a4db2a0d6
aruzevich <78572020+aruzevich@users.noreply.github.com>
2021-03-03 20:17:02 -0500

    name and style change

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYXJ1emV2aWNoIiwiYSI6ImNrbGxmdWVlcjBjdHcyb28xN
 
 var options = {
   container: 'mapContainer', // container ID
-  style: 'mapbox://styles/mapbox/streets-v11', // style URL
+  style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
   center: [-73.96969, 40.77807], // starting position [lng, lat]
   zoom: 9 // starting zoom
 }
@@ -20,35 +20,35 @@ var parks = [
   {
     name: 'Central Park',
     location: 'Where: The Ramble',
-    borough: 'Borough: Manhattan',
+    borough: 'Manhattan',
     point: [-73.96969, 40.77807],
     color: '#5B5EA6'
   },
   {
     name: 'Van Cortland Park',
     location: 'Where: John Kieran Trail',
-    borough: 'Borough: The Bronx',
+    borough: 'The Bronx',
     point: [-73.89137, 40.89155],
     color: '#B565A7'
   },
   {
     name: 'Prospect Park',
     location: 'Where: The Ravine',
-    borough: 'Borough: Brooklyn',
+    borough: 'Brooklyn',
     point: [-73.96883, 40.66412],
     color: '#DD4124'
   },
   {
     name: 'Highland Park',
     location: 'Where: Ridgewood Reservoir',
-    borough: 'Borough: Queens',
+    borough: 'Queens',
     point: [-73.88736, 40.68628],
     color: '#009B77'
   },
   {
     name: 'Clove Lakes Park',
     location: 'Where: Martling Lake',
-    borough: 'Borough: Staten Island',
+    borough: 'Staten Island',
     point: [-74.11644, 40.62328],
     color: '#EFC050'
   }
@@ -56,9 +56,9 @@ var parks = [
 
 parks.forEach(function(park) {
   var html = `
-    <h2>${park.name}</h2>
-    <div><b>${park.location}</b></div>
-    <div>${park.borough}</div>
+    <h3>${park.borough}</h3>
+    <div><b>${park.name}</b></div>
+    <div>${park.location}</div>
   `
 
   new mapboxgl.Marker({
